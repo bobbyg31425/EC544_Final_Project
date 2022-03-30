546ac05e62bfc81a35a88d3bc56cf1184bf30af6
bobbyg31425 <96958618+bobbyg31425@users.noreply.github.com>
2022-03-29 21:10:30 -0400

    added file to take picture via webcam and save it on local computer

---
+++ OpenCV/photo_capture.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 29 20:35:02 2022
+
+@author: bobby
+"""
+
+# Importing libraries
+import numpy as np
+import cv2 as cv
+import os
+from matplotlib import pyplot as plt
+
+# Connect to capture device
+cap = cv.VideoCapture(0)
+ret, frame = cap.read()
+
+# Show and save capture
+# Note: plt.imshow flips R and B color channels since
+# capture is in RGB format, but OpenCV looks for BGR format 
+plt.imshow(frame)
+cv.imwrite('webcam_photo.jpg',frame)
+
+# Releases camera from being 'in use'
+cap.release()
